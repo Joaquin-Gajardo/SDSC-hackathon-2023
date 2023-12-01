a7b85644df1cbd0141d2f1dc355e7281acad684a
Joaquin Gajardo <jgajardo@ethz.ch>
2023-12-01 04:54:32 +0000

    fix typos with previous commit

--- src/datasets.py
+++ src/datasets.py
@@ -3,18 +3,25 @@ from PIL import Image
 import numpy as np
 
 import torchvision
+from torchvision.transforms.functional import pil_to_tensor
 from torchvision.transforms import Compose, ToTensor, CenterCrop
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 
 from src import ROOT_DIR
+from src.utils import (
+    is_bbox_outside_crop,
+    get_patch_label,
+    unpack_yolo_label,
+    yolo_bbox_relative_to_absolute_coords,
+    )
 
 
 DATA_DIR = ROOT_DIR / 'data' / 'raw'
 OUT_IMAGE_RES = 1280
 
 
-class MixedDataset(Dataset):
+class MixedDatasetPreprocessing(Dataset):
 
     dataset = 'mixed'
 
@@ -26,6 +33,7 @@ class MixedDataset(Dataset):
         self.images_dir = self.dataset_dir / 'images'
         self.labels_dir = self.dataset_dir / 'yolo'
         self.patches_dir = DATA_DIR / 'patches'
+        #self.original_H, self.original_W = pil_to_tensor(Image.open(self.images[idx])).shape[1:]
         
         # File paths
         self.images = [path for path in self.images_dir.iterdir()]
@@ -37,23 +45,25 @@ class MixedDataset(Dataset):
         if not all([Path(path).exists() for path in self.patches]):
             self._extra_paths_setup()
             
-        # Discard labels outside crop boundaries and update images, labels and patches attributes accordingly (only 9 so we just discard them)
-        self.patches_idxs_to_discard = self._patches_idxs_to_discard()
-        self.images = [path for i, path in enumerate(self.images) if i not in self.patches_idxs_to_discard]
-        self.labels = [path for i, path in enumerate(self.labels) if i not in self.patches_idxs_to_discard]
-        self.patches = [path for i, path in enumerate(self.patches) if i not in self.patches_idxs_to_discard]
-    
         # Define transforms
         self.transforms = Compose([ToTensor()])
+        
+        # Discard labels outside crop boundaries and update images, labels and patches attributes accordingly (only 9 so we just discard them)
         if center_crop:
-            self.transforms.transforms.insert(1, CenterCrop(OUT_IMAGE_RES)), # NOTE: If doing this we would need to exclude labels and patches that fall outside the crop
+            self.patches_idxs_to_discard = self._patches_idxs_to_discard() # NOTE: requires iterating over the dataset
+            self.images = [path for i, path in enumerate(self.images) if i not in self.patches_idxs_to_discard]
+            self.labels = [path for i, path in enumerate(self.labels) if i not in self.patches_idxs_to_discard]
+            self.patches = [path for i, path in enumerate(self.patches) if i not in self.patches_idxs_to_discard]
+
+            # Now we can apply center crop form now on
+            self.transforms.transforms.insert(len(self.transforms.transforms), CenterCrop(OUT_IMAGE_RES))
 
     def _patches_idxs_to_discard(self):
         """Rerturns a list of indexes of the patches ((bboxes) that fall outside the crop boundaries.
         This can be later also used for augmentation as well as the train set, only val set would be exluded."""
         patches_to_discard = []
-        for i in range(len(dataset)):
-            image, label, patch = dataset[i]
+        for i in range(len(self)):
+            image, label, patch = self[i]
             
             # There are more than one bbox per image, we need to find which one is the one that we see in the patch
             # Finding closest match by patch size:  
